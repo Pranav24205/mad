2da27f48d269a87c887e04b0853e644cb5cefeb0
Pranav24205 <96657174+Pranav24205@users.noreply.github.com>
2022-01-05 20:22:39 +0530

    Update __init__.py

--- SuperBot/__init__.py
+++ SuperBot/__init__.py
@@ -22,7 +22,7 @@ if Var.STRING_SESSION:
     bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
 else:
     session_name = "startup"
-    bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
+    bot = TelegramClient(session_name, Var.API_ID, Var.API_HASH)
     
 
 Lastupdate = time.time()
