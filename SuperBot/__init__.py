e11a014602a805f0e80ee3838d1554f927c3c8b1
Pranav24205 <96657174+Pranav24205@users.noreply.github.com>
2022-01-05 20:23:32 +0530

    Update __init__.py

--- SuperBot/__init__.py
+++ SuperBot/__init__.py
@@ -19,7 +19,7 @@ os.system("pip3 install aria2p")
 
 if Var.STRING_SESSION:
     session_name = str(Var.STRING_SESSION)
-    bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
+    bot = TelegramClient(StringSession(session_name), Var.API_ID, Var.API_HASH)
 else:
     session_name = "startup"
     bot = TelegramClient(session_name, Var.API_ID, Var.API_HASH)
